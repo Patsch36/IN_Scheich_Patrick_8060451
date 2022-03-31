3690930af200184061b1ebd0a338e40f77f48777
Patsch36 <scheichpatrick@gmail.com>
2022-03-31 21:06:13 +0200

    Excercise 1- integrate numDiff finished

--- Abgabe_1/Runscript.m
+++ Abgabe_1/Runscript.m
@@ -2,6 +2,5 @@
 % myNewton('function', @myPoly, 'derivative', @dmyPoly, 'startValue', 4, ...
 % 'maxIter', 100, 'feps', 10e-10, 'livePlot', 'on')
 
-
 myNewton('function', @myPoly, 'startValue', 4, ...
     'maxIter', 100, 'feps', 1e-10, 'livePlot', 'on')
