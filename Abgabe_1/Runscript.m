28e2b4ad74f9736a7a5df7e1c4249823d14626a0
Patsch36 <scheichpatrick@gmail.com>
2022-03-31 21:09:04 +0200

    Added Runscript Comment

--- Abgabe_1/Runscript.m
+++ Abgabe_1/Runscript.m
@@ -1,4 +1,6 @@
 
+% Runscrpt for testinf Newton Algorithm using myPoly function and no derivate function
+
 % myNewton('function', @myPoly, 'derivative', @dmyPoly, 'startValue', 4, ...
 % 'maxIter', 100, 'feps', 10e-10, 'livePlot', 'on')
 
